ba5e19efa10a97556e6630e4ee3507826996d214
chun950yi <chun950yi@gmail.com>
2022-01-27 16:43:32 +0800

    remove NextButton in QuestionsViewController and Fix auto next question after select answer

--- Demo0805/Controller/QuestionsViewController.swift
+++ Demo0805/Controller/QuestionsViewController.swift
@@ -15,7 +15,7 @@ class QuestionsViewController: UIViewController {
     var questionsArray :[String] = []
     var correctAnswerArray :[String] = []
     var incorrectAnswerArray :[String] = []
-    var q = 1
+    var questionCount = 1
     var score = 0
     var checkAnswerNumber = 0
     let stopWatch = ShowTime()
@@ -25,24 +25,16 @@ class QuestionsViewController: UIViewController {
     @IBOutlet weak var elapsedTimeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
-    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet var choiceBtnOutlet: [UIButton]!
     
-    @IBOutlet weak var buttonA: UIButton!
-    @IBOutlet weak var buttonB: UIButton!
-    @IBOutlet weak var buttonC: UIButton!
-    @IBOutlet weak var buttonD: UIButton!
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         getQuestions()
         
-        nextButton.layer.cornerRadius = 10
-        buttonA.layer.cornerRadius = 10
-        buttonB.layer.cornerRadius = 10
-        buttonC.layer.cornerRadius = 10
-        buttonD.layer.cornerRadius = 10
+        for i in 0...3 {
+            choiceBtnOutlet[i].layer.cornerRadius = 10
+            choiceBtnOutlet[i].addTarget(self, action: #selector(selectAnswerAction), for: .touchUpInside)
+        }
        
     }
     
@@ -141,14 +133,17 @@ class QuestionsViewController: UIViewController {
                         for i in 0...9 {
                             questionsArray.append(questionArray.results[i].question)
                             correctAnswerArray.append(questionArray.results[i].correct_answer)
-                            incorrectAnswerArray.append(questionArray.results[i].incorrect_answers[0])
-                            incorrectAnswerArray.append(questionArray.results[i].incorrect_answers[1])
-                            incorrectAnswerArray.append(questionArray.results[i].incorrect_answers[2])
+                            for j in 0...2{
+                                incorrectAnswerArray.append(questionArray.results[i].incorrect_answers[j])
+                            }
                         }
                         DispatchQueue.main.async {
                             self.firstQuestionAnswer(nil)
-                            Timer.scheduledTimer(timeInterval: 0.1, target: self,
-                                selector: #selector(QuestionsViewController.updateElapsedTimeLabel(_:)), userInfo: nil, repeats: true)
+                            Timer.scheduledTimer(timeInterval: 0.1,
+                                                 target: self,
+                                                 selector: #selector(QuestionsViewController.updateElapsedTimeLabel(_:)),
+                                                 userInfo: nil,
+                                                 repeats: true)
                             stopWatch.start()
                         }
                     }
@@ -164,7 +159,7 @@ class QuestionsViewController: UIViewController {
         
     }
     
-    @IBAction func firstQuestionAnswer(_ sender: UIButton?) {
+    func firstQuestionAnswer(_ sender: UIButton?) {
         var answerArray:[String] = []
         answerArray.append(correctAnswerArray[0])
         answerArray.append(incorrectAnswerArray[0])
@@ -172,172 +167,67 @@ class QuestionsViewController: UIViewController {
         answerArray.append(incorrectAnswerArray[2])
         answerArray.shuffle()
         questionLabel.text = questionsArray[0]
-        buttonA.setTitle(answerArray[0], for: .normal)
-        buttonB.setTitle(answerArray[1], for: .normal)
-        buttonC.setTitle(answerArray[2], for: .normal)
-        buttonD.setTitle(answerArray[3], for: .normal)
+        
+        for i in 0...3 {
+            choiceBtnOutlet[i].setTitle(answerArray[i], for: .normal)
+            
+        }
+        
+    }
+    
+    @objc func selectAnswerAction(_ sender: UIButton){
+        print(correctAnswerArray[questionCount-1])
+        print((sender.titleLabel?.text)!)
+        
+        if sender.titleLabel?.text == correctAnswerArray[questionCount-1]{
+            sender.backgroundColor = .green
+            
+            if checkAnswerNumber >= 3 {
+                score += 30
+            } else {
+                score += 10
+            }
+            checkAnswerNumber += 1
+            scoreLabel.text = String(score)
+        } else {
+            sender.backgroundColor = .red
+            //要把正確答案找出來，並亮綠色
+            for choicebtn in 0..<choiceBtnOutlet.count {
+                if choiceBtnOutlet[choicebtn].titleLabel?.text == correctAnswerArray[questionCount-1] {
+                    choiceBtnOutlet[choicebtn].backgroundColor = .green
+                    break
+                }
+            }
+            
+            if checkAnswerNumber >= 3 {
+                score -= 10
+            }
+            checkAnswerNumber = 0
+            scoreLabel.text = String(score)
+        }
+        
+        //選完答案之後
+        DispatchQueue.main.asyncAfter( deadline: DispatchTime.now() + 1 ) {
+            if self.questionCount >= 10 {
+                self.stopWatch.stop()
+                self.performSegue(withIdentifier: "score", sender: nil)
+            } else {
+                var answerArray: [String] = []
+                answerArray.append(self.correctAnswerArray[0+self.questionCount])
+                answerArray.append(self.incorrectAnswerArray[0+3*self.questionCount])
+                answerArray.append(self.incorrectAnswerArray[1+3*self.questionCount])
+                answerArray.append(self.incorrectAnswerArray[2+3*self.questionCount])
+                answerArray.shuffle()
+                self.questionLabel.text = self.questionsArray[0+self.questionCount]
+                for i in 0...3 {
+                    self.choiceBtnOutlet[i].setTitle(answerArray[i], for: .normal)
+                    self.choiceBtnOutlet[i].backgroundColor =  UIColor(red: 225, green: 214, blue: 179, alpha: 1)
 
-        nextButton.isUserInteractionEnabled = false
-        
-    }
-    
-    @IBAction func nextQuestion(_ sender: Any) {
-        
-        if q >= 10 {
-            stopWatch.stop()
-            performSegue(withIdentifier: "score", sender: nil)
-        } else {
-            var answerArray: [String] = []
-            answerArray.append(correctAnswerArray[0+q])
-            answerArray.append(incorrectAnswerArray[0+3*q])
-            answerArray.append(incorrectAnswerArray[0+1*q])
-            answerArray.append(incorrectAnswerArray[2+3*q])
-            answerArray.shuffle()
-            questionLabel.text = questionsArray[0+q]
-            buttonA.setTitle(answerArray[0], for: .normal)
-            buttonA.backgroundColor = UIColor(red: 225, green: 214, blue: 179, alpha: 1)
-            buttonB.setTitle(answerArray[1], for: .normal)
-            buttonB.backgroundColor = UIColor(red: 225, green: 214, blue: 179, alpha: 1)
-            buttonC.setTitle(answerArray[2], for: .normal)
-            buttonC.backgroundColor = UIColor(red: 225, green: 214, blue: 179, alpha: 1)
-            buttonD.setTitle(answerArray[3], for: .normal)
-            buttonD.backgroundColor = UIColor(red: 225, green: 214, blue: 179, alpha: 1)
-            q += 1
-            numberOfQuestionLabel.text = String(q)
-            nextButton.isUserInteractionEnabled = false
+                }
+                self.questionCount += 1
+                self.numberOfQuestionLabel.text = String(self.questionCount)
+            }
         }
-    }
-    
-    @IBAction func selectA(_ sender: UIButton) {
-       
-        if buttonA.titleLabel?.text == correctAnswerArray[q-1] {
-            buttonA.backgroundColor = .green
-            
-            if checkAnswerNumber >= 3 {
-                score += 30
-            } else {
-                score += 10
-            }
-            checkAnswerNumber += 1
-            scoreLabel.text = String(score)
-        } else {
-            switch correctAnswerArray[q-1] {
-            case buttonB.titleLabel?.text:
-                buttonB.backgroundColor = .green
-            case buttonC.titleLabel?.text:
-                buttonC.backgroundColor = .green
-            case buttonD.titleLabel?.text:
-                buttonD.backgroundColor = .green
-            default:
-                break
-            }
-            buttonA.backgroundColor = .red
-            if checkAnswerNumber >= 3 {
-                score -= 10
-            }
-            checkAnswerNumber = 0
-            scoreLabel.text = String(score)
-        }
-        nextButton.isUserInteractionEnabled = true
-    }
-    
-    @IBAction func selectB(_ sender: UIButton) {
-       
-        if buttonB.titleLabel?.text == correctAnswerArray[q-1] {
-            buttonB.backgroundColor = .green
-            
-            if checkAnswerNumber >= 3 {
-                score += 30
-            } else {
-                score += 10
-            }
-            checkAnswerNumber += 1
-            scoreLabel.text = String(score)
-        } else {
-            switch correctAnswerArray[q-1] {
-            case buttonA.titleLabel?.text:
-                buttonA.backgroundColor = .green
-            case buttonC.titleLabel?.text:
-                buttonC.backgroundColor = .green
-            case buttonD.titleLabel?.text:
-                buttonD.backgroundColor = .green
-            default:
-                break
-            }
-            buttonB.backgroundColor = .red
-            if checkAnswerNumber >= 3 {
-                score -= 10
-            }
-            checkAnswerNumber = 0
-            scoreLabel.text = String(score)
-        }
-        nextButton.isUserInteractionEnabled = true
-    }
-    
-    @IBAction func selectC(_ sender: UIButton) {
-       
-        if buttonC.titleLabel?.text == correctAnswerArray[q-1] {
-            buttonC.backgroundColor = .green
-            
-            if checkAnswerNumber >= 3 {
-                score += 30
-            } else {
-                score += 10
-            }
-            checkAnswerNumber += 1
-            scoreLabel.text = String(score)
-        } else {
-            switch correctAnswerArray[q-1] {
-            case buttonB.titleLabel?.text:
-                buttonB.backgroundColor = .green
-            case buttonA.titleLabel?.text:
-                buttonA.backgroundColor = .green
-            case buttonD.titleLabel?.text:
-                buttonD.backgroundColor = .green
-            default:
-                break
-            }
-            buttonC.backgroundColor = .red
-            if checkAnswerNumber >= 3 {
-                score -= 10
-            }
-            checkAnswerNumber = 0
-            scoreLabel.text = String(score)
-        }
-        nextButton.isUserInteractionEnabled = true
-    }
-    
-    @IBAction func selectD(_ sender: UIButton) {
-       
-        if buttonD.titleLabel?.text == correctAnswerArray[q-1] {
-            buttonD.backgroundColor = .green
-            
-            if checkAnswerNumber >= 3 {
-                score += 30
-            } else {
-                score += 10
-            }
-            checkAnswerNumber += 1
-            scoreLabel.text = String(score)
-        } else {
-            switch correctAnswerArray[q-1] {
-            case buttonB.titleLabel?.text:
-                buttonB.backgroundColor = .green
-            case buttonC.titleLabel?.text:
-                buttonC.backgroundColor = .green
-            case buttonA.titleLabel?.text:
-                buttonA.backgroundColor = .green
-            default:
-                break
-            }
-            buttonD.backgroundColor = .red
-            if checkAnswerNumber >= 3 {
-                score -= 10
-            }
-            checkAnswerNumber = 0
-            scoreLabel.text = String(score)
-        }
-        nextButton.isUserInteractionEnabled = true
     }
     
     
